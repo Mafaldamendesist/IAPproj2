e9e951f01ae0f39a3657f0e6523acc21a7f72e4e
MargaridaMorais <maguy.1997@gmail.com>
2018-11-17 12:22:47 +0000

    the beginning

--- BN.py
+++ BN.py
@@ -9,24 +9,54 @@ Created on Mon Oct 15 15:51:49 2018
 
 class Node():
     def __init__(self, prob, parents = []):
-        pass
-    
-    def computeProb(self, evid):
-        pass
+        self.prob = prob
+        self.parents = parents
 
-        return 0
-    
+    def computeProb(self, evid):
+        if(self.parents.len() > 1){
+            left_evid = evid[self.parents[0]];
+            right_evid = evid[self.parents[1]];
+
+            prob_true = self.prob[left_evid][right_evid]
+            prob_false = 1 - prob_true
+
+            prob = [prob_false, prob_true]
+
+            return prob
+        }
+
+        elif(self.parents.len() == 1) {
+            parent_evid = evid[self.parents]
+
+            prob_true = self.prob[parent_evid]
+            prob_false = 1 - prob_true
+
+            prob = [prob_false, prob_true]
+
+            return prob
+        }
+
+        else {
+            prob_true = self.prob
+            prob_false = 1 - prob_true
+
+            prob = [prob_false, prob_true]
+
+            return prob
+        }
+
 class BN():
     def __init__(self, gra, prob):
-        pass
+        self.gra = gra
+        self.prob = prob
 
     def computePostProb(self, evid):
         pass
-               
+
         return 0
-        
-        
+
+
     def computeJointProb(self, evid):
         pass
-        
+
         return 0
