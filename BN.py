e1be47430f86ddfd6390098e434cad9bc6d7a128
Mafaldamendesist <mafalda.mendes@tecnico.ulisboa.pt>
2018-11-17 12:42:53 +0000

    first class

--- BN.py
+++ BN.py
@@ -8,13 +8,34 @@ Created on Mon Oct 15 15:51:49 2018
 
 
 class Node():
-    def __init__(self, prob, parents = []):
-        pass
-    
+    def __init__(self, prob, parents = []): 
+        self.parents = parents
+        self.prob = prob
+        self.result = []
+        
+      
+        
     def computeProb(self, evid):
-        pass
-
-        return 0
+        if(len(self.parents)>1):
+            left_evid = evid[self.parents[0]]
+            right_evid =evid[self.parents[1]]
+            
+            prob_true = self.prob[left_evid][right_evid]
+            prob_false = 1 - prob_true
+            self.result.append(prob_false);
+            self.result.append(prob_true); 
+        elif(len(self.parents) == 1):
+            parents_evid = evid[self.parents[0]]
+            prob_true = self.prob[0][parents_evid]
+            self.result.append(prob_true)
+        else:
+            prob_true = self.prob[0]
+            prob_false = 1 - prob_true
+            self.result.append(prob_false);
+            self.result.append(prob_true);
+    
+    def __repr__(self):
+        return "Baye results: {}".format(self.result) 
     
 class BN():
     def __init__(self, gra, prob):
